23e3ee35c9f8f79b52a0113f7d9000a986c0121b
Avinash <avinash.birkh@gmail.com>
2020-09-30 17:13:45 +0530

    name changed

--- Farmera/src/main/java/com/lti/FarmeraApplication.java
+++ Farmera/src/main/java/com/lti/FarmeraApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ArtifactFarmerApplication {
+public class FarmeraApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ArtifactFarmerApplication.class, args);
+		SpringApplication.run(FarmeraApplication.class, args);
 	}
 
 }
